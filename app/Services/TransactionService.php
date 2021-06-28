4a428d9e97e303422b3e384f1b97e8dcc7c72a91
Camila Pereira <camila379.cp@gmail.com>
2021-06-28 01:35:01 -0300

    add roles for transaction

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -2,77 +2,187 @@
 
 namespace App\Services;
 
+use App\Repositories\StatusRepository;
 use App\Repositories\TransactionRepository;
 use App\Repositories\TypeRepository;
-use App\Repositories\UsersRepository;
+use App\Repositories\UserRepository;
 use App\Repositories\WalletRepository;
+use App\Enums\TransactionEnum;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class TransactionService
 {
     private $transactionRepository;
     private $walletRepository;
-    private $usersRepository;
+    private $userRepository;
     private $typeRepository;
-    
-    public function __construct(TransactionRepository $transactionRepository, WalletRepository $walletRepository, UsersRepository $usersRepository, TypeRepository $typeRepository) {
+    private $statusRepository;
+
+    public function __construct(TransactionRepository $transactionRepository, WalletRepository $walletRepository, userRepository $userRepository, TypeRepository $typeRepository, StatusRepository $statusRepository)
+    {
         $this->transactionRepository = $transactionRepository;
         $this->walletRepository = $walletRepository;
-        $this->usersRepository = $usersRepository;
+        $this->userRepository = $userRepository;
         $this->typeRepository = $typeRepository;
+        $this->statusRepository = $statusRepository;
     }
 
-    public function store(Array $params)
+    public function getAll()
+    {
+        $transaction = $this->transactionRepository->getAll();
+        $response = [
+            'message' => TransactionEnum::SUCESS_FIND,
+            'error' => 0,
+            'transaction' => $transaction
+        ];
+        if (empty($transaction)) {
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_NOT_DATA;
+            return $response;
+        }
+        return $response;
+    }
+
+    public function get(int $id)
+    {
+        $transaction = $this->transactionRepository->get($id);
+        $response = [
+            'message' => TransactionEnum::SUCESS_FIND,
+            'error' => 0,
+            'transaction' => $transaction
+        ];
+        if (empty($transaction)) {
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_NOT_FOUND;
+            return $response;
+        }
+        return $response;
+    }
+
+    public function create(array $params)
     {
         DB::begintransaction();
-        $this->transactionRepository->store($params);
-        $payerWallet = $this->walletRepository->show($params["payer"]);
-        $payeeWallet = $this->walletRepository->show($params["payee"]);
-        $payerUser = $this->usersRepository->show($payerWallet["id_users"]);
+
+        $payerWallet = $this->walletRepository->get($params["payer"]);
+        $payeeWallet = $this->walletRepository->get($params["payee"]);
+        $payerUser = $this->userRepository->get($payerWallet["id_users"]);
+
+        $response = [
+            'message' => TransactionEnum::SUCESS_FIND,
+            'error' => 0,
+            'transaction' => null
+        ];
 
         if (!empty($payerWallet->wallet)) {
-            return "carteira do pagador não existe";
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_NOT_EXIST_PAYER;
+            return $response;
         }
 
         if (!empty($payeeWallet->wallet)) {
-            return "carteira do recebedor não existe";
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_NOT_EXIST_PAYEE;
+            return $response;
         }
 
-        if(!$this->isPermitted($payerUser->type_id)){
-            return "Tipo de usuario não permite realizar esse tipo de transação";
+        if (!$this->isPermitted($payerUser->type_id)) {
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_NOT_TYPE_PERMITED;
+            return $response;
         }
 
-        if($params["value"] > $payerWallet->value) {
-            return "saldo insuficiente";
+        if ($params["value"] > $payerWallet->value) {
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_INSUFFICIENT_AMOUNT;
+            return $response;
+        }
+
+        $transactionEfect = $this->transactionRepository->create($params);
+
+        if (empty($transactionEfect)) {
+            DB::rollback();
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_TRANSACTION_NOT_CREATED;
+            return $response;
+        }
+        $statusParams = ['transaction_id' => $transactionEfect['id'], 'status' => 'Pendente'];
+        $this->statusRepository->create($statusParams);
+
+        $autorizada = $this->isAuthorizable();
+
+        if (!$autorizada) {
+            $statusParams = ['transaction_id' => $transactionEfect['id'], 'status' => 'Not Authorized'];
+            $this->statusRepository->create($statusParams);
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_TRANSACTION_NOT_AUTHORIZED;
+            return $response;
         }
 
         $payerWalletValue = $payerWallet->value - $params["value"];
 
-        $payerUpdated = $this->walletRepository->update($payerWallet->id, $payerWalletValue); 
-        if(!$payerUpdated) {
+        $payerUpdated = $this->walletRepository->save($payerWallet->id, $payerWalletValue);
+        if (!$payerUpdated) {
             DB::rollback();
-            return "Erro ao atualizar dados do pagador";
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_PAYER_NOT_UPDATED;
+            return $response;
         }
-        
+
         $payeeWalletValue = $payeeWallet->value + $params["value"];
-        
-        $payeeUpdated = $this->walletRepository->update($payeeWallet->id, $payeeWalletValue); 
-        if(!$payeeUpdated) {
+
+        $payeeUpdated = $this->walletRepository->save($payeeWallet->id, $payeeWalletValue);
+        if (!$payeeUpdated) {
             DB::rollback();
-            return "Erro ao atualizar dados do beneficiário";
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_PAYEE_NOT_UPDATED;
+            return $response;
         }
+
+        $statusParams = ['transaction_id' => $transactionEfect['id'], 'status' => 'Authorized'];
+        $this->statusRepository->create($statusParams);
 
         DB::commit();
 
-        return "Transação efetuada com sucesso";
+        if (!$this->sendNotification()) {
+            $response['error'] = 1;
+            $response['message'] = TransactionEnum::ERR_NOT_SEND_NOTIFICATION;
+            return $response;
+        }
+
+        return [
+            'message' => TransactionEnum::SUCESS_CREATED,
+            'error' => 0,
+            'transaction' => $transactionEfect
+        ];
     }
 
     public function isPermitted(int $id)
     {
-        $validateType = $this->typeRepository->show($id);  
-        if($validateType['id'] === 1){
+        $validateType = $this->typeRepository->get($id);
+        if ($validateType["id"] === 1) {
             return false;
         }
         return true;
+    }
+
+    public function isAuthorizable()
+    {
+        $response = Http::post("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6");
+        if (isset($response["message"]) && !empty($response["message"]) && $response["message"] === "Autorizado") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function sendNotification()
+    {
+        $response = Http::post("http://o4d9z.mocklab.io/notify");
+        if (isset($response["message"]) && !empty($response["message"]) &&  $response["message"] === "Success") {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
