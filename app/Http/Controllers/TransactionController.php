6b0cbeaf1323b63af54dc5d9c8b0cf8d7664224c
Camila Pereira <camila379.cp@gmail.com>
2021-06-28 01:27:28 -0300

    removed variables

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,43 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Transaction;
-use App\Models\Users;
-use App\Models\Wallet;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
 use App\Services\TransactionService;
 
 class TransactionController extends Controller
 {
     private $transactionService;
-    private $wallet;
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(TransactionService $transactionService, Wallet $wallet)
+    public function __construct(TransactionService $transactionService)
     {
         $this->transactionService = $transactionService;
-        $this->wallet = $wallet;
     }
 
     public function show($id)
     {
-        $transaction = $this->transactionService->find($id);
+        $transaction = $this->transactionService->get($id);
         return response()->json($transaction, status: Response::HTTP_OK);
     }
 
     public function index()
     {
-        $transaction = $this->transactionService->all();
+        $transaction = $this->transactionService->getAll();
         return response()->json($transaction, status: Response::HTTP_OK);
     }
 
-    public function store(Request $request, Users $users, Wallet $wallet)
+    public function store(Request $request)
     {
         $validatedData = $this->validate($request, [
             'value' => 'required|numeric',
@@ -46,8 +40,6 @@ class TransactionController extends Controller
             'payer' => 'required|int|different:payee|exists:wallet,id'
         ]);
 
-        return response($this->transactionService->store($request->all()), Response::HTTP_CREATED);
+        return response($this->transactionService->create($request->all()), status: Response::HTTP_CREATED);
     }
-
-    //
 }
