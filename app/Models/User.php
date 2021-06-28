2df7e28880a6be7681688e5ea02e9f17196eddc9
Camila Pereira <camila379.cp@gmail.com>
2021-06-28 01:40:59 -0300

    removed variables

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,32 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model
 {
-    use Authenticatable, Authorizable, HasFactory;
+    protected $table = "users";
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email',
+        "name",
+        "email",
+        "password",
+        "cpf_cnpj",
+        "type_id"
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
     ];
+
+    public function type()
+    {
+        return $this->hasOne(Type::class);
+    }
+
+    public function walletValue()
+    {
+        return $this->hasMany(Wallet::class, 'id_users', 'id');
+    }
 }
