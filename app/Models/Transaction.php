8ef478dc0fa4996d1f8ee2de675e2cb92b747dc8
Camila Pereira <camila379.cp@gmail.com>
2021-06-28 01:33:34 -0300

    add relation with status of transactions

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
+    protected $table = "transaction";
+
     protected $fillable = [
         "payee",
         "payer",
@@ -20,5 +22,10 @@ class Transaction extends Model
     public function payer()
     {
         return $this->hasOne(Users::class, "id", "payer");
+    }
+
+    public function transactionStatus()
+    {
+        return $this->hasMany(StatusTransaction::class, 'transaction_id', 'id');
     }
 }
